52d3cc482a3c9e64ea0d5830b815f0718384f79a
Abdullah khan <abdullahadil108@gmail.com>
2026-06-23 23:05:42 +0500

    Time: 0 ms (100%), Space: 12.4 MB (68.87%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        strs.sort()
+        if not strs:
+            return ""
+
+        first=strs[0]
+        last=strs[-1]
+        ans=[]
+        
+        for i in range(min(len(first), len(last))):
+            if first[i]!=last[i]:
+                return ''.join(ans)
+
+            ans.append(first[i])
+        return ''.join(ans)
+            
